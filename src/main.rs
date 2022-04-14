f804c67d464c3ac5b965287529533d22cda3c769
william wong <william@freepackets.net>
2022-04-15 06:54:45 +0800

    Testcase: List
    https://doc.rust-lang.org/stable/rust-by-example/hello/print/print_display/testcase_list.html

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,23 @@
-/*
-https://doc.rust-lang.org/stable/rust-by-example/hello/print/print_display.html
-*/
 use std::fmt;
-#[derive(Debug)]
-struct MinMax(i64, i64);
 
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
+struct List(Vec<i32>);
 
-#[derive(Debug)]
-struct Complex {
-    real: f64,
-    imag: f64,
-}
-
-impl fmt::Display for Point2D {
+impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: {}", count, v)?;
+        }
+        write!(f, "]")
     }
 }
-
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
-
-impl fmt::Display for Complex {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} + {}i", self.real, self.imag)
-    }
-}
-
 fn main() {
-    let minmax = MinMax(0, 14);
-
-    println!("Compare structures:");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range = MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-
-    println!(
-        "The big range is {big} and the small is {small}",
-        small = small_range,
-        big = big_range
-    );
-
-    let point = Point2D { x: 3.3, y: 7.2 };
-
-    let point = Point2D { x: 3.3, y: 7.2 };
-
-    println!("Compare points");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
-
-    let complex = Complex {
-        real: 3.3,
-        imag: 7.2,
-    };
-    println!("Display: {}", complex);
-    println!("Debug:{:?}", complex);
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }

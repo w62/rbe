5e85ffb03dcbf5b34e0c88fb4b776f578e154ba0
william wong <william@freepackets.net>
2022-04-18 17:44:12 +0800

    [Primiatives] (https://doc.rust-lang.org/stable/rust-by-example/primitives.html)
    [Literals and operators]
    (https://doc.rust-lang.org/stable/rust-by-example/primitives/literals.html)

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,51 @@
-use std::fmt::{self, Display, Formatter};
-
-struct City {
-    name: &'static str,
-    // Latitude
-    lat: f32,
-    // Longitude
-    lon: f32,
-}
-
-impl Display for City {
-    // `f` is a butter, and this method must write the formatted string into it
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
-        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
-
-        write!(
-            f,
-            "{}:{:.3}°{} {:.3}°{}",
-            self.name,
-            self.lat.abs(),
-            lat_c,
-            self.lon.abs(),
-            lon_c
-        )
-    }
-}
-
-#[derive(Debug)]
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
-}
-
-impl Display for Color {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(
-            f,
-            "RGB({}, {}, {}) 0x{:02X}{:02X}{:02X}",
-            self.red, self.green, self.blue, self.red, self.green, self.blue
-        )
-    }
-}
-
 fn main() {
-    for city in [
-        City {
-            name: "Dublin",
-            lat: 53.347778,
-            lon: -6.259722,
-        },
-        City {
-            name: "Oslo",
-            lat: 59.95,
-            lon: 10.75,
-        },
-        City {
-            name: "Vancouver",
-            lat: 49.25,
-            lon: -123.1,
-        },
-    ]
-    .iter()
-    {
-        println!("{}", *city);
-    }
+    // Variables can be type annotaged.
+    let logical: bool = true;
 
-    for color in [
-        Color {
-            red: 128,
-            green: 255,
-            blue: 90,
-        },
-        Color {
-            red: 0,
-            green: 3,
-            blue: 254,
-        },
-        Color {
-            red: 0,
-            green: 0,
-            blue: 0,
-        },
-    ]
-    .iter()
-    {
-        println!("{:?}", *color);
-        println!("{}", *color);
-    }
+    let a_float: f64 = 1.0; // Regular annotation
+    let an_integer = 5i32; // Suffix annotation
+
+    // Or a default will be used.
+    let default_float = 3.0; // `f64`
+    let default_integer = 7; // `i32`
+
+    // A type can also be inferred from context
+    let mut inferred_type = 12; // Type i64 is inferred from another line
+    inferred_type = 4294967296i64;
+
+    // A mutable variable's value can be changes.
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+
+    // Error! The type of a variable can't be changed.
+    //    mutable = true;
+
+    // Variables can be overwritten with shadowing.
+    let mutable = true;
+
+    println!("1 + 2 = {}", 1u32 + 2);
+    println!("1 - 2 = {}", 1i32 - 2);
+    // TODO ^ Try changing `1i32` to `iu32` to see why the type is important
+    // Tried. The complier complains about potential overflow.
+    //
+    /*
+       |
+    27 |     println!("1 - 2 = {}", 1u32 - 2);
+       |                            ^^^^^^^^ attempt to compute `1_u32 - 2_u32`, which would overflow
+       |
+       = note: `#[deny(arithmetic_overflow)]` on by default
+       */
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 and 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 or 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("1 << 5 i s {}", 1u32 << 5);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
 }
